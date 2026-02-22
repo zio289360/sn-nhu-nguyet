9a62fabf730a17852c7b346ba6cb115c0bd4afe1
zio289360 <anguyen2893@gmail.com>
2026-02-22 17:32:28 +0700

    Add base path configuration to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/sn-nhu-nguyet/', // <--- THÊM DÒNG NÀY VÀO ĐÂY
+  base: '/sn-nhu-nguyet/',
   plugins: [react()],
 })
